5a95d1bc07e22a70ae4135ea28c8d5c283b312df
kmomar1 <kmomar1@asu.edu>
2026-02-13 23:30:23 -0700

    Created starter api creation file structure

---
+++ backend/controllers/GithubController.js
@@ -0,0 +1,5 @@
+import { StatusCodes } from "http-status-codes";
+
+export const cloneRepo = (req, res) => {
+
+}
---
+++ backend/routes/GithubRoutes.js
@@ -0,0 +1,6 @@
+import { Router } from "express"
+import { cloneRepo } from "../controllers/GithubController" 
+
+const router = Router()
+
+export default router
