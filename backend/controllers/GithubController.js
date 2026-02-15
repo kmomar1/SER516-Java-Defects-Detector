b65c86ff84f44e3d39666fd12933afacf4464160
sbinsalman <233876342+sbinsalm@users.noreply.github.com>
2026-02-15 09:51:28 -0700

    feature: added the ability to clone repoes from github and store it to the local folder
    {
      "github_link": "https://github.com/username/repo.git"
    }

--- backend/controllers/GithubController.js
+++ backend/controllers/GithubController.js
@@ -1,5 +1,17 @@
-import { StatusCodes } from "http-status-codes";
+import shell from 'shelljs';
+
 
 export const cloneRepo = (req, res) => {
+    const githubLink = req.body.github_link;
+    const repoName = githubLink.split('/').pop();
 
-}
+
+
+    //Need to change it later to add the repo to the volume of the container
+    const repoPath = `./repos/${repoName}`;
+    if (shell.exec(`git clone ${githubLink} ${repoPath}`).code !== 0){
+        return res.status(500).json({ message: 'Failed to clone repository' });
+    }else{
+        return res.status(200).json({ message: 'Repository cloned successfully' });
+    }
+}   
