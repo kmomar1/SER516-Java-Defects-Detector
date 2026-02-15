5bdee8ec13c6b80fd9e51163d9f73c19f941725b
sbinsalman <233876342+sbinsalm@users.noreply.github.com>
2026-02-15 09:51:28 -0700

    feature: added the ability to clone repoes from github and store it to the local folder
    {
      "github_link": "https://github.com/username/repo.git"
    }

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import githubRoutes from "./routes/GithubRoutes.js"
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const server = express()
 
 server.use(cors())
 server.use(express.json())
+
+server.use("/api/github", githubRoutes)
 
 server.listen(PORT, () => console.log(`Running on port ${PORT}`))
 
