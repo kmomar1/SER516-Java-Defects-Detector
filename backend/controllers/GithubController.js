ac4381f978616ab09957df1a05ffd6aeb4b49bba
kmomar1 <kmomar1@asu.edu>
2026-02-15 14:32:35 -0700

    refactor: Remove redundancy and formatting adjustment
    - Add repo folder to gitignore
    - Adjust code spacing and error message response

--- backend/controllers/GithubController.js
+++ backend/controllers/GithubController.js
@@ -4,14 +4,13 @@ import shell from 'shelljs';
 export const cloneRepo = (req, res) => {
     const githubLink = req.body.github_link;
     const repoName = githubLink.split('/').pop();
-
-
+    const repoPath = `./repos/${repoName}`;
 
     //Need to change it later to add the repo to the volume of the container
-    const repoPath = `./repos/${repoName}`;
     if (shell.exec(`git clone ${githubLink} ${repoPath}`).code !== 0){
-        return res.status(500).json({ message: 'Failed to clone repository' });
-    }else{
-        return res.status(200).json({ message: 'Repository cloned successfully' });
+      return res.status(401).json({ message: 'Failed to clone repository please make sure the URL is correct and the repo is public' });
+    }
+    else {
+      return res.status(200).json({ message: 'Repository cloned successfully' });
     }
 }   
