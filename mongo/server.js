dffc6e807e7c40c723a9962b558d08259b0da8af
kmomar1 <kmomar1@asu.edu>
2026-02-26 16:37:12 -0700

    feat: Mongo starter service

---
+++ mongo/server.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8081
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.listen(PORT, () => console.log(`Running on port ${PORT}`))
+
