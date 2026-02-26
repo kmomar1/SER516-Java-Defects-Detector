136d3b760e78310c8d3ce0659916a44c9782273c
sbinsalman <233876342+sbinsalm@users.noreply.github.com>
2026-02-25 20:48:16 -0700

    added MongoDB connection code
    Don't forget to update the .env with
    databaseURL

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import mongoose from "mongoose";
 import githubRoutes from "./routes/GithubRoutes.js"
 
 dotenv.config()
@@ -10,6 +11,17 @@ const server = express()
 
 server.use(cors())
 server.use(express.json())
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Health check route
+server.get("/health", (req, res) => {
+  res.status(200).type("text/plain").send("Server works");
+  console.log("Server works")
+});
 
 server.use("/api/github", githubRoutes)
 
