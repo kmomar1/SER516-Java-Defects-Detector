346c7dd63418e25ae1d2ea82f52a3348a3af370a
sbinsalman <233876342+sbinsalm@users.noreply.github.com>
2026-02-26 20:06:25 -0700

    added the mongodb connection

--- mongo/server.js
+++ mongo/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
+import mongoose from "mongoose";
 
 dotenv.config()
 
@@ -9,6 +10,18 @@ const server = express()
 
 server.use(cors())
 server.use(express.json())
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Health check route
+server.get("/health", (req, res) => {
+  res.status(200).type("text/plain").send("Server works");
+  console.log("Server works")
+});
+
 
 server.listen(PORT, () => console.log(`Running on port ${PORT}`))
 
